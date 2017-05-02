dde6178c9755d47adecb6979703ec48cc71006d1
arska2 <aarni.seppanen@gmail.com>
2017-05-02 08:48:37 +0300

    testing

---
+++ Pre-assignment#3/network_measures.m
@@ -0,0 +1,31 @@
+%In this pre-assignment the task is to test the Brain Connectivity Toolbox
+%The macaque network from https://sites.google.com/site/bctnet/datasets was
+clear;
+
+%load the dataset
+load('macaque71.mat');
+
+%% Degree and Similarity
+
+% Degree:
+% degrees_und.m (BU, WU networks); degrees_dir.m (BD, WD networks).
+
+% 
+% Strength: 
+% strengths_und.m (WU networks); strengths_dir.m (WD networks).
+% strengths_und_sign.m (WU signed networks).
+
+% Joint degree
+% jdegree.m (BD, WD networks).
+
+
+% Topological overlap: 
+% gtom.m (BU networks).
+
+% 
+% Neighborhood overlap: 
+% edge_nei_overlap_bu.m (BU networks); edge_nei_overlap_bd.m (BD networks).
+
+
+% Matching index: 
+% matching_ind_und.m (BU networks); matching_ind.m (BD networks).
